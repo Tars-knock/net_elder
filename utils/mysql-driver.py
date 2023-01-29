d195e7b1b5cc8b5dedd74ffab5bf0fab0c6726c3
Tars-knock <sfly13344@163.com>
2023-01-29 22:39:54 +0800

    add: some bug

--- utils/mysql-driver.py
+++ utils/mysql-driver.py
@@ -1,6 +1,20 @@
 import configparser
+from datetime import datetime
 
-from sqlalchemy import create_engine
+from pymysql import TIMESTAMP
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+Base = declarative_base()
+
+
+class Url(Base):
+    __tablename__ = 'url'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    url = Column(String(200))
+    title = Column(String(200))
+    description = Column(String(200))
+    create_time = Column('timestamp', TIMESTAMP, nullable=False, default=datetime.now())
 
 
 class MysqlDriver:
@@ -17,3 +31,26 @@ class MysqlDriver:
             "mysql+pymysql://{}:{}@{}:{}/{}".format(config.get("mysql", "name"), config.get("mysql", "passwd"),
                                                     config.get("mysql", "host"), config.get("mysql", "port"),
                                                     config.get("mysql", "dbname")))
+        print(self.engine.url)
+
+    def test_insert(self):
+        data_tars = pd.DataFrame({
+            'id': [2],
+            'url': ['https://tars-knock.cn'],
+            'title': ['Tars blog'],
+            'description': ['Tars赛博小窝']})
+        data_tars.to_sql('url', self.engine, index=False)
+
+    def test_select(self):
+        DBSession = sessionmaker(self.engine)
+        session = DBSession()
+        # Base.to_dict = to_dict
+        rows = session.query(Url).all()
+        print([row.to_dict() for row in rows])
+        session.close()
+
+
+if __name__ == '__main__':
+    connection = MysqlDriver()
+    connection.test_select()
+    # connection.test_insert()
