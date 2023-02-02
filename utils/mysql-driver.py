fe7b5d6fb6e0fd14335fbcc93f0744090de9e326
Tars-knock <sfly13344@163.com>
2023-02-02 14:28:12 +0800

    complete: DAO

--- utils/mysql-driver.py
+++ utils/mysql-driver.py
@@ -8,12 +8,54 @@ from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
 
+# 数据库链接类，处理对MySQL的增删改查
+class Driver:
+    def __init__(self):
+        config = configparser.ConfigParser()
+        # 从配置文件读取数据库相关设置
+        config.read("../mysql-config.ini", encoding="utf-8")
+        config.sections()
+        config.options("mysql")
+        # 初始化数据库连接
+        # 按实际情况依次填写MySQL的用户名、密码、IP地址、端口、数据库名
+        self.engine = create_engine(
+            "mysql+pymysql://{}:{}@{}:{}/{}".format(config.get("mysql", "name"), config.get("mysql", "passwd"),
+                                                    config.get("mysql", "host"), config.get("mysql", "port"),
+                                                    config.get("mysql", "dbname")))
+        print(f'connecting {self.engine.url}')
+
+    def insert_url(self, url):
+        with Session(self.engine) as session:
+            session.add(url)
+            session.commit()
+
+    def test_select(self):
+        with Session(self.engine) as session:
+            # urls = select(Url).where(Url.id.__eq__(1))
+            urls = select(Url)
+            for url in session.scalars(urls):
+                print(url)
+            # 注意scalars与scalar的区别
+            # print(session.scalar(urls))
+
+    def test_insert(self):
+        with Session(self.engine) as session:
+            data1 = Url(
+                url='docs.sqlalchemy.org',
+                title='sqlalchemy documentation',
+                description='sqlalchemy orm quick start'
+            )
+            session.add(data1)
+            session.commit()
+
+
 class Base(DeclarativeBase):
     type_annotation_map = {
         datetime.datetime: TIMESTAMP(timezone=True),
     }
 
 
+# 数据库实体类
 class Url(Base):
     __tablename__ = "url"
 
@@ -28,30 +70,7 @@ class Url(Base):
         return f"Url(id={self.id!r}, title={self.title!r}, createTime={self.create_time}, url={self.url!r})"
 
 
-class Driver:
-    def __init__(self):
-        config = configparser.ConfigParser()
-        # 从配置文件读取数据库相关设置
-        config.read("../mysql-config.ini", encoding="utf-8")
-        config.sections()
-        config.options("mysql")
-        # 初始化数据库连接
-        # 按实际情况依次填写MySQL的用户名、密码、IP地址、端口、数据库名
-        self.engine = create_engine(
-            "mysql+pymysql://{}:{}@{}:{}/{}".format(config.get("mysql", "name"), config.get("mysql", "passwd"),
-                                                    config.get("mysql", "host"), config.get("mysql", "port"),
-                                                    config.get("mysql", "dbname")))
-        print(self.engine.url)
-
-    def test_select(self):
-        session = Session(self.engine)
-        # urls = select(Url).where(Url.id.__eq__(1))
-        urls = select(Url)
-        # for url in session.scalar(urls):
-        #     print(url)
-        print(session.scalar(urls))
-
-
 if __name__ == '__main__':
     driver = Driver()
+    # driver.test_insert()
     driver.test_select()
